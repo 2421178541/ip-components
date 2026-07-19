9151d7940a91b3150e72e644e12a5da88c3f2244
242 <2421178541@qq.com>
2026-07-19 21:16:12 +0800

    修复闪退

--- app/src/main/java/com/ipwidget/MainActivity.kt
+++ app/src/main/java/com/ipwidget/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.ipwidget
 
+import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,7 +15,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        // 每次从后台回来刷新日志（如果小组件后台写入了新内容）
         findViewById<TextView>(R.id.logView).text = AppLogger.getLog()
     }
 }
